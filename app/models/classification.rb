10af096eb75f84c2a13ffbd76bd02bc5299c3357
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-02-26 03:42:36 +0500

    Adding total amount for transaction

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -5,4 +5,12 @@ class Classification < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :classtrans, class_name: 'ClassTran'
   has_many :transactions, through: :classtrans
+
+  def total_amount
+    total = 0
+    classtrans.each do |item|
+      total += item.trans_table.amount
+    end
+    total
+  end
 end
