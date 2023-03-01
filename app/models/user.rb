7bf815607f04f9c546498157afbd153b81bec2ee
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-03-02 01:26:32 +0500

    Adding integration test for home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :photo, presence: true
   has_many :classifications
   has_many :transactions
 end
--- spec/models/classification_spec.rb
+++ spec/models/classification_spec.rb
@@ -2,12 +2,14 @@ require_relative '../rails_helper'
 
 RSpec.describe Classification, type: :model do
   before do
-    @user = User.create(name: 'Sajeel Zafar', email: 'sajeelzafar1995@gmail.com', photo: 'https://media.licdn.com/dms/image/D4D35AQHT5r7V2Hc8QA/profile-framedphoto-shrink_200_200/0/1669286916513?e=1678291200&v=beta&t=tdlpYVNZ3BVHutaGERgXdZibmsfBOJiEVlfsMyaSvMI')
+    @user = User.create(id: 5, name: 'Sajeel Zafar', email: 'sajeelzafar1995@gmail.com', photo: 'https://media.licdn.com/dms/image/D4D35AQHT5r7V2Hc8QA/profile-framedphoto-shrink_200_200/0/1669286916513?e=1678291200&v=beta&t=tdlpYVNZ3BVHutaGERgXdZibmsfBOJiEVlfsMyaSvMI')
     @classification = Classification.create(
+      id: 4,
       name: 'Fruits',
       icon: 'https://www.shutterstock.com/image-vector/url-shortener-man-pushes-address-600w-2201694049.jpg',
       author_id: @user.id
     )
+    @transaction = TransTable.create(id: 1, name: 'Grapes', amount: 2.10, author_id: @user.id)
   end
 
   it 'should have a name' do
